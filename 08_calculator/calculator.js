13f84e08a743c2fbe5fa91e6e72489997ac606a0
zhivko <ji.rusev@gmail.com>
2024-09-03 17:46:49 +0300

    Calculator completed.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,37 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((curent, number) => {
+    return curent + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((curent, number) => {
+    return curent * number;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (number, pow) {
+  return Math.pow(number, pow);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let product = 1;
+  for (let i = 1; i <= number; i++) {
+    product *= i;
+  }
+  return product;
 };
+
+
+console.log(sum([1, 2, 3]));
 
 // Do not edit below this line
 module.exports = {
